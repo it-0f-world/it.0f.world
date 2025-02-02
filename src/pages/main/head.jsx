4c70bba31a2e86df4b670c69e2d1418c01cfffda
Bob Nem <noname.from.milkyway@gmail.com>
2025-02-02 20:42:58 +1000

    multilanguage intefration start

--- src/pages/main/head.jsx
+++ src/pages/main/head.jsx
@@ -1,6 +1,7 @@
 import style from './head.module.css';
 import VideoBG from './media/videoplayback.mp4';
 import VideoPoster from './media/poster.webp';
+import { IoLanguage } from "react-icons/io5";
 
 export default function Head() {
     return (
@@ -10,24 +11,34 @@ export default function Head() {
                     autoPlay
                     loop
                     muted
-                    className={style.videoBackground}
                     poster={VideoPoster}
+                    className={style.videoBackground}
                 >
                     <source src={VideoBG} type="video/mp4" />
                 </video>
             </div>
             <header className={style.header}>
-                <div className={style.centered}>
-                    <h1 className={style.title}>{"[ SPACE OF IT DECISIONS ]"}</h1>
-                    <p className={style.description}>{"< Information Technologies Development, Artistic  & Technical Design />"}</p>
+                <div className={style.top}>
+                    <button className={style.button1}><IoLanguage className={style.icon} /></button>
                 </div>
-                <div className={style.buttons}>
-                    <button className={style.button}>Coding</button>
-                    <button className={style.button}>Design</button>
-                    <button className={style.button}>Portfolio</button>
-                    <button className={style.button}>Articles</button>
-                    <button className={style.button}>Contacts</button>
+                <div className={style.center}>
+                    <div className={style.left} />
+                    <div className={style.slogan}>
+                        <h1 className={style.title}>{"[ SPACE OF IT DECISIONS ]"}</h1>
+                        <p className={style.description}>{"< Information Technologies Development, Artistic  & Technical Design />"}</p>
+                    </div>
+                    <div className={style.right}>
+                        <div className={style.buttons}>
+                            <button className={style.button}>Coding</button>
+                            <button className={style.button}>Design</button>
+                            <button className={style.button}>Promotion</button>
+                            <button className={style.button}>Portfolio</button>
+                            <button className={style.button}>Articles</button>
+                            <button className={style.button}>Contacts</button>
+                        </div>
+                    </div>
                 </div>
+                <div className={style.bottom} />
             </header>
         </div>
     );
