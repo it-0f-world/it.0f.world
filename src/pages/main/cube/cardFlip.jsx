8e94416f8018585894fc6784bc628c5ebb55e85e
Bob Nem <noname.from.milkyway@gmail.com>
2025-02-17 21:10:08 +1000

    flip card completed

--- src/pages/main/cube/cardFlip.jsx
+++ src/pages/main/cube/cardFlip.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './cardFlip.module.css'
+import React from 'react'
 
 const cardFlip = () => {
-    
     return (
-        <div>
-            <div>
-                
+        <div className='w-full h-full flex place-content-center'>
+            <div className='w-[280px] bg-transparent cursor-pointer group rounded-2xl perspective-1000'>
+                <div className='flex relative w-full h-full duration-500 preserve-3d group-hover:rotate-y-180 transform-style-preserve-3d'>
+                    <div className='w-full h-full absolute rounded-2xl overflow-hidden p-3 border border-[#4fafe3]'>
+                        <h3 className='text-3xl text-center'>Web-browser</h3>
+                        <p className='pt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, suscipit?</p>
+                    </div>
+                    <div className='w-full h-full absolute rounded-2xl rotate-y-180 overflow-hidden bg-[#2f4f4f] space-y-5 p-3 bg-opacity-95 backface-hidden  border border-[#4fafe3]'>
+                        <h3 className='text-3xl text-center'>React</h3>
+                        <p className='pt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, suscipit?</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/main/cube/crossPlatformApplications.jsx
+++ src/pages/main/cube/crossPlatformApplications.jsx
@@ -4,10 +4,17 @@ import CardFlip from './cardFlip'
 
 function crossPlatformApplications() {
   return (
-    <div className={style.wrapper}>
+    <div className='flex flex-col grow'>
+      <div className={style.slogan}>
         <h2 className={style.header}>Cross-platform applications development.</h2>
-        <p>Web-sites, desktop, mobile. For all most useful operating systems - Linux, Windows, Android, IOS, HarmonyOS. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis debitis neque quaerat illum explicabo dicta et obcaecati asperiores, in, tempore aspernatur nihil veniam a. Accusantium error id libero, repellendus possimus minus et laborum?</p>
-        <CardFlip />
+        <p>Web-sites, desktop, mobile. For all most useful operating systems - Linux, Windows, Android, IOS, HarmonyOS.</p>
+      </div>
+      <div className='flex grow pt-4'>
+        <p className=''>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur modi reiciendis quae autem architecto maiores ea maxime debitis. Aperiam dolor, mollitia officiis ea laborum, est consequuntur illo quasi tempore enim magni! Aperiam, neque.</p>
+        <div className={style.cards}>
+          <CardFlip />
+        </div>
+      </div>
     </div>
   )
 }
